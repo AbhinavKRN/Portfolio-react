1405fa11830f79724917ae0e7fd22ad215b0a47f
AbhinavKRNarayan <115357208+AbhinavKRN@users.noreply.github.com>
2024-06-16 23:28:10 +0530

    Update Projects.jsx

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,11 @@
 import styles from './ProjectsStyles.module.css';
 import viberr from '../../assets/viberr.png';
 import ProjectCard from '../../common/ProjectCard';
-
+import editor from '../../assets/editor.png';
+import music from '../../assets/music.jpg';
+import kanban from '../../assets/kanban.jpeg';
+import quiz from '../../assets/quiz.jpg';
+import webb from '../../assets/webscapper.png';
 
 function Projects() {
   return (
@@ -9,31 +13,31 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={music}
           link="https://github.com/AbhinavKRN/MusicStoreManagement"
           h3="MusicStoreManagement"
           p="Backend project using SpringBoot to make a music store management system."
         />
         <ProjectCard
-          src={viberr}
+          src={editor}
           link="https://github.com/AbhinavKRN/ImageEditor"
           h3="Image Editor"
           p="A backend project using JavaFX -> you can simply edit your images using this codebase!"
         />
         <ProjectCard
-          src={viberr}
+          src={webb}
           link="https://github.com/AbhinavKRN/Web-Scraping-and-Comparing-Prices-on-Amazon-and-Snapdeal"
           h3="WebScrapper"
           p="Python project in which user types the product to compare it from two different websites i.e. Amazon and Snapdeal."
         />
         <ProjectCard
-          src={viberr}
+          src={kanban}
           link="https://github.com/AbhinavKRN/Kanban_Board"
           h3="Kanban Board"
           p=" It allows users to add tasks with different priorities and manage them on a Kanban board"
         />
         <ProjectCard
-          src={viberr}
+          src={quiz}
           link="https://github.com/AbhinavKRN/Quiz-App"
           h3="Quiz App"
           p="This provides a platform where user can give quiz."
